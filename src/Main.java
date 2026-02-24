8d226a48aaffe34f815c27a84a36d5383ab9681e
MinhNN <minhnguyennhatt@gmail.com>
2026-02-24 15:06:23 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        Drink[] drinks = new Drink[3];
+
+        drinks[0] = new Coffee("C001", "Bạc sỉu", 30000, true);
+        drinks[1] = new FruitJuice("FJ001", "Nước cam", 40000, 10);
+        drinks[2] = null;
+
+        // Vòng lặp duyệt mảng để in thông tin hóa đơn
+        for (int i = 0; i < drinks.length; i++) {
+            if (drinks[i] != null) {
+                drinks[i].displayInfo();
+                System.out.println("Thành tiền: " + drinks[i].calculatePrice());
+
+                if (drinks[i] instanceof IMixable) {
+                    ((IMixable) drinks[i]).mix();
+                }
+                System.out.println("---");
+            }
+        }
+    }
+}
